93e44cb8e59eff8602302cb157d2c50ff30f6548
Larry Kluger <larry.kluger@docusign.com>
2020-09-06 16:51:43 +0300

    Add docs link

--- src/components/NavbarSection/NavbarSection.jsx
+++ src/components/NavbarSection/NavbarSection.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 function NavbarSection(props) {
       
     return (
     <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="#home">DocuSign JSON to SDK tool</Navbar.Brand>
+        <Navbar.Brand href="">DocuSign JSON to SDK tool</Navbar.Brand>
+        <Nav className="ml-5">
+            <Nav.Link href="https://github.com/docusign/docusign-php-client/blob/master/README.md" 
+                target="_blank">Documentation</Nav.Link>
+        </Nav>
+
     </Navbar>
     )
 }
